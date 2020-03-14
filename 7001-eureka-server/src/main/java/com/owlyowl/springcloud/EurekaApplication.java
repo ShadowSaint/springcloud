d03310c57ab8e552faf6db83a1c4b18de054468f
ShadowSaint <30032777@qq.com>
2020-03-14 14:31:42 +0800

    eureka start up

--- 7001-eureka-server/src/main/java/com/owlyowl/springcloud/EurekaApplication.java
+++ 7001-eureka-server/src/main/java/com/owlyowl/springcloud/EurekaApplication.java
@@ -2,13 +2,12 @@ package com.owlyowl.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderApplication {
-
+@EnableEurekaServer
+public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
