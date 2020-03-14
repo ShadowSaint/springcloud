323c1f5fbbfc69c82323bb2d26e5b9a21a949478
ShadowSaint <30032777@qq.com>
2020-03-14 15:36:26 +0800

    provider replicas

---
+++ 8002-provider-payment/src/main/java/com/owlyowl/springcloud/service/PaymentService.java
@@ -0,0 +1,7 @@
+package com.owlyowl.springcloud.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.owlyowl.springcloud.entry.domain.PaymentDO;
+
+public interface PaymentService extends IService<PaymentDO> {
+}
