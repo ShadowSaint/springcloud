27cd71b8452b4752d836de9053d9335f362df963
ShadowSaint <30032777@qq.com>
2020-03-17 08:37:50 +0800

    openfeign start up

---
+++ 80-consumer-feign-order/src/main/java/springcloud/controller/OrderController.java
@@ -0,0 +1,38 @@
+package springcloud.controller;
+
+import com.owlyowl.springcloud.entry.vo.CommonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import springcloud.entry.domain.PaymentDO;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+@Api("下订单")
+public class OrderController {
+
+//    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @SuppressWarnings("rawtypes")
+    @ApiOperation("插入")
+    @PostMapping("/")
+    public CommonResult create(@RequestBody PaymentDO paymentDO) {
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/",paymentDO,CommonResult.class);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @ApiOperation("查询")
+    @GetMapping("/")
+    public CommonResult getPayment(@RequestParam String id) {
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/?id="+id,CommonResult.class);
+    }
+}
---
+++ 80-consumer-feign-order/src/main/java/springcloud/entry/domain/PaymentDO.java
@@ -0,0 +1,14 @@
+package springcloud.entry.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDO {
+
+    private Long id;
+    private String serial;
+}
